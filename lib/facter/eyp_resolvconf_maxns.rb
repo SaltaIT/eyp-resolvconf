efc48f55c466980f0225954c3338255caf2483d6
Jordi Prats <jordi.prats@gmail.com>
2019-06-11 10:02:05 +0200

    typo

--- lib/facter/eyp_resolvconf_maxns.rb
+++ lib/facter/eyp_resolvconf_maxns.rb
@@ -1,8 +1,8 @@
 max_ns = Facter::Util::Resolution.exec('grep "define MAXNS" /usr/include/resolv.h 2>/dev/null | sed \'s/.*MAXNS\\s\\+\\([0-9]\\+\\).*/\\1/\'')
-unless cgroup.nil? or cgroup.empty?
+unless max_ns.nil? or max_ns.empty?
   Facter.add('eyp_resolvconf_maxns') do
       setcode do
-
+        max_ns
       end
   end
 end
